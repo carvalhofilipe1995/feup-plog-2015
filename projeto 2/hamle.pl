6d5705ad536da78beac135b986bd525a2f98dca6
Diogo Carvalho <diogoavc@gmail.com>
2015-12-14 01:27:56 +0000

    Create Board and get Possibilities Init

--- projeto 2/hamle.pl
+++ projeto 2/hamle.pl
@@ -47,30 +47,57 @@ generateBoard(Dimension, Counter, Board) :-
         length(Row, Dimension),
         NewCounter is Counter - 1,
         generateBoard(Dimension, NewCounter, T1),
-        append([Row], T1, Board).
+        append([Row], T1, Board).	
 
 % Solver
 
-solve([HRow | TRows], Sol):-
-        getBlackPieces(Board, BlackList),
-        length(BlackList, BlackNumber),
-        length(HRow, Dimension),
-        length(Cols, BlackNumber),
-        length(Rows, BlackNumber),
-        domain(Cols, 1, Dimension),
-        domain(Rows, 1, Dimension),
-        tryToSolve(Cols, Rows, BlackList),
-        append(Sol, SolFlat)
-		labeling([], SolFlat).
+solve_board(Board, Sol):-
+        length(Board, Dimension),
+        create_board(Sol, Dimension).
+        %get_black_pieces(Board, BlackList),
+        %get_possibilities(Board, Dimension, PossibList),
+        %adjacents(Sol),
+        %restrict(Sol, PossibList),
+        %append(Sol, SolFlat)
+		%labeling([], SolFlat).
 
 
+create_board(Board, N):-
+        				length(Board, N),
+        				maplist(set_length(N), Board).
+
+set_length(L, Ls) :- length(Ls, L).
 
 
-tryToSolve([Col1 | Cols], [Row1 | Rows], BlackList):-
-        tryToSolvePiece(Col1, Row1, BlackList),
-        tryToSolve(Cols, Rows, BlackList).
+get_possib(Board, Dim, List):-
+							get_possib_row(Board, Dim, 1, List).
 
 
+get_possib_row(Board, Dim, Row, List):-
+										get_possib_cell(Board, Dim, Row, 1, List).
+
+
+get_possib_cell(Board, Dim, Dim, Dim, List):-
+											make_possib_list(Board, Dim, Dim, Dim, TempList),
+											append([], [[TempList]], List).
+
+
+get_possib_cell(Board, Dim, Row, Dim, List):-
+											make_possib_list(Board, Dim, Row, Dim, TempList),
+											NewRow is Row + 1,
+											get_possib_cell(Board, Dim, NewRow, 1, RestList),
+											append([[TempList]], RestList, List).
+
+
+get_possib_cell(Board, Dim, Row, Col, List):-
+											make_possib_list(Board, Dim, Row, Col, TempList),
+											NewCol is Col + 1,
+											get_possib_cell(Board, Dim, Row, NewCol, RestList),
+											append([[TempList]], RestList, List).
+
+
+make_possib_list(Board, Dim, Row, Col, List):-
+											getCell(Board, Row, Col, Cell, Dim).
 
 
 /* Check Range Black pieces */
@@ -112,33 +139,33 @@ calcEdgeDistances(Dimension, Row, Col, Result):- DistanceRight is Dimension - Co
 % Solver
 
 /* Get Black Pieces */
-getBlackPieces([Hrow | Trows], List) :-
+get_black_pieces([Hrow | Trows], List) :-
         length(Hrow, Dimension),
-        getBlackPieces([Hrow | Trows], Dimension, 1, List).
+        get_black_pieces([Hrow | Trows], Dimension, 1, List).
 
-getBlackPieces([], _, _, []).
+get_black_pieces([], _, _, []).
 
-getBlackPieces([Hrow | Trows], Dimension, Row, List) :-
-        getBlackPiecesRow(Hrow, Dimension, Row, RowList),
+get_black_pieces([Hrow | Trows], Dimension, Row, List) :-
+        get_black_pieces_row(Hrow, Dimension, Row, RowList),
         NewRow is Row + 1,
-        getBlackPieces(Trows, Dimension, NewRow, BoardList),
+        get_black_pieces(Trows, Dimension, NewRow, BoardList),
         append(RowList, BoardList, List).
 
-getBlackPiecesRow(RowList, Dimension, Row, List):-
-        getBlackPiecesRow(RowList, Dimension, Row, 1, List).
+get_black_pieces_row(RowList, Dimension, Row, List):-
+        get_black_pieces_row(RowList, Dimension, Row, 1, List).
 
-getBlackPiecesRow(RowList, Dimension, Row, Dimension, List):-
+get_black_pieces_row(RowList, Dimension, Row, Dimension, List):-
         nth1(Dimension, RowList, Number),
         (Number > 0 -> append([], [[Row, Dimension, Number]], TempList)
         ;
         append([],[],TempList)),
         append([], TempList, List).
 
-getBlackPiecesRow(RowList, Dimension, Row, Col, List):-
+get_black_pieces_row(RowList, Dimension, Row, Col, List):-
         nth1(Col, RowList, Number),
         (Number > 0 -> append([], [[Row, Col, Number]], TempList)
         ;
         append([],[],TempList)),
         NewCol is Col + 1,
-        getBlackPiecesRow(RowList, Dimension, Row, NewCol, RestList),
+        get_black_pieces_row(RowList, Dimension, Row, NewCol, RestList),
         append(TempList, RestList, List).
